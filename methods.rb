30ebbbf83897f33b7e3e9df3c1d0827613e45f91
leah wanjiku <leagm22@gmail.com>
2023-02-07 13:33:04 +0300

    done

--- methods.rb
+++ methods.rb
@@ -1,1 +1,33 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet
+    puts "Hello, name!"
+end
+
+def greet (name )
+    puts "Hello, #{name}!"
+  end
+  greet "jimmy"
+  
+  def greet_with_default(name='programmer')
+    puts "Hello, #{name}!"
+  end
+  greet_with_default "Naureen"
+
+  def add(num1,num2)
+    return num1 + num2
+  end
+  sum=add(2,5)
+  def halve(number)
+    if number.class != Integer
+      return nil
+    end
+  
+    number/2
+  end
+  halve(6)
+
+  
